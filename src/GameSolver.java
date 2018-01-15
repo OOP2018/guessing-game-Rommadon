dc66e0aac695b733aeafcbb5371ada717451400a
GDROM <Rommadon.t@ku.th>
2018-01-15 14:39:34 +0700

    Finnish binary search

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -8,31 +8,33 @@ public class GameSolver {
 
 	/** play the game. */
 	/**
-	 * the play method plays a game using random for find solver.
+	 * the play method plays a game using binarysearch for find solver.
 	 * @param guess is number that random automatic
 	 * @return answer
 	 */
 	public int play(NumberGame game) {
 		Scanner console = new Scanner(System.in);
 		boolean correct = false ;
-		long seed = System.nanoTime(); 
-		Random rand = new Random(seed);
-		int guess = rand.nextInt(100) + 1;
+
+		int guess =0 ;
+		int min=0,max=100;
+		
 		while(!correct){
 		correct = game.guess(guess);
 		
-//		random number from upper and lower range that message tell 
-		
-			if(game.getMessage().equals("Woah! Your answer is WAY too small.")){
-			guess = rand.nextInt(100-guess) + guess;
-			}else if(game.getMessage().equals("Sorry, too small.")){
-			guess = rand.nextInt(100-guess) + guess;
+
+			
+			
+			guess = min+(max-min)/2;
+			if(game.getSecret()>guess){
+				min=guess;
+				
+			}else
+			{
+				max=guess;
 			}
-			if(game.getMessage().equals("No way! Your answer is WAY too large.")){
-			guess = rand.nextInt(guess-1) + 1;
-			}else{
-			guess = rand.nextInt(guess-1) + 1;
-			}
+			 
+
 			
 		}
 		
